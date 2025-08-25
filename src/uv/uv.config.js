446ff809b8b018eb2670f926fce275f135597e3d
Atomix <139202621+Atomix0925@users.noreply.github.com>
2025-08-25 13:47:23 -0400

    Update uv.config.js

--- src/uv/uv.config.js
+++ src/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/src/service/',
-    bare:'https://smart-eagle-351.nowgg.me/bare/',
+    prefix: '/src/atomix/',
+    bare:'https://b.shuttlemath.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/src/uv/uv.handler.js',
